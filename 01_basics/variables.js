d869435d868b1425da4b938993a917622f0eb5da
Adrinil Sahoo <131552713+Adrinil@users.noreply.github.com>
2026-04-05 11:00:55 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12;
+let accountEmail = "asd@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
+let accountState ;
+
+//accountId = 13;
+
+console.log(accountId);
+
+/* var is not used now because it can be redeclared and it have an issue with function scope and block scope.
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
